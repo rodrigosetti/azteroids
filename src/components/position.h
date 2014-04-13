7f23e03bcee59284a7ed2a77f36476b2fc4c0144
Rodrigo Setti <rodrigosetti@gmail.com>
2014-04-12 19:42:38 -0700

    Collisions part 1: position correction

--- src/components/position.h
+++ src/components/position.h
@@ -13,6 +13,10 @@ struct Position : entityx::Component<Position> {
             rotation_axis_y(rotation_axis_y),
             rotation_axis_z(rotation_axis_z) {}
 
+    float distance(entityx::ptr<Position> other) {
+        return sqrt(pow(other->x - x, 2) + pow(other->y - y, 2));
+    }
+
     float x, y, rotation, rotation_axis_x, rotation_axis_y, rotation_axis_z;
 };
 
--- src/systems/collision.cpp
+++ src/systems/collision.cpp
@@ -1,8 +1,40 @@
+#include <components/geometry.h>
+#include <components/mass.h>
+#include <components/momentum.h>
+#include <components/position.h>
+
 #include "collision.h"
 
 void CollisionSystem::update(entityx::ptr<entityx::EntityManager> entities,
                              entityx::ptr<entityx::EventManager> events,
                              double dt) {
 
+    for (auto entity1 : entities->entities_with_components<Position, Geometry>()) {
+        entityx::ptr<Position> pos1 = entity1.component<Position>();
+        entityx::ptr<Geometry> geo1 = entity1.component<Geometry>();
+
+        for (auto entity2 : entities->entities_with_components<Position, Geometry>()) {
+            if (entity1 != entity2) {
+                entityx::ptr<Position> pos2 = entity2.component<Position>();
+                entityx::ptr<Geometry> geo2 = entity2.component<Geometry>();
+
+                double distance = pos1->distance(pos2);
+                double min_distance = geo1->radius + geo2->radius;
+
+                if (distance < min_distance) {
+                    // Collision!
+                    double dx = pos2->x - pos1->x;
+                    double dy = pos2->y - pos1->y;
+                    double adjust = (min_distance - distance) / 2.0;
+
+                    // correct position to the minimum distance
+                    pos1->x -= dx / distance * adjust;
+                    pos1->y -= dy / distance * adjust;
+                    pos2->x += dx / distance * adjust;
+                    pos2->y += dy / distance * adjust;
+                }
+            }
+        }
+    }
 }
 
